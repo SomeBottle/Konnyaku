6b1cf00fdebc88e858c932cd5e81e27f0dca10e5
SomeBottle <somebottle@outlook.com>
2025-02-11 01:18:23 +0800

    feat(utils): add bangumi info extractor

--- konnyaku/bangumi.py
+++ konnyaku/bangumi.py
@@ -6,7 +6,7 @@ Bangumi API 接入模块
 
 import requests
 
-
+from time import sleep
 from konnyaku.config import BANGUMI_API_KEY
 
 BANGUMI_API_URL = "https://api.bgm.tv"
@@ -27,10 +27,8 @@ class BangumiAPI:
         :param api_key: Bangumi API Key，如果不提供则从环境变量中读取
         """
         self.api_key = BANGUMI_API_KEY
-        print('API KEY', self.api_key)
         if api_key:
             self.api_key = api_key
-        print('API KEY', self.api_key)
 
     def _get(self, url: str) -> dict:
         """
@@ -64,7 +62,9 @@ class BangumiAPI:
         :return: 番剧角色信息
         :raises requests.HTTPError: 请求异常
         """
-        url = f"{BANGUMI_API_URL}/{BANGUMI_API_VERSION}/subjects/{subject_id}/characters"
+        url = (
+            f"{BANGUMI_API_URL}/{BANGUMI_API_VERSION}/subjects/{subject_id}/characters"
+        )
         return self._get(url)
 
     def get_character_info(self, character_id: str) -> dict:
@@ -77,3 +77,63 @@ class BangumiAPI:
         """
         url = f"{BANGUMI_API_URL}/{BANGUMI_API_VERSION}/characters/{character_id}"
         return self._get(url)
+
+    def get_subject_all_character_infos(self, subject_id: str) -> list[dict]:
+        """
+        提取 Bangumi 番剧中的所有角色的信息，主要是名字、关系
+
+        :param subject_id: 番剧 ID
+        :return: 角色名列表 [{relation: str, name: str, name_chs: str, gender: str}, ...]
+        :raises requests.HTTPError: 请求异常
+        """
+        RATE_LIMIT = 10  # 每经过多少次请求就休息一下
+
+        characters = self.get_subject_characters(subject_id)
+        character_ids = []
+        # 临时存储角色在番剧中的关系
+        id_to_relation = {}
+        for character in characters:
+            if "id" in character:
+                character_ids.append(character["id"])
+                if "relation" in character:
+                    id_to_relation[character["id"]] = character["relation"]
+
+        result = []
+        request_count = 0
+        for character_id in character_ids:
+            if (request_count + 1) % RATE_LIMIT == 0:
+                # 休息一下
+                print("Let me take a break...ε-(´-ᗜ-`;)", end="\r")
+                sleep(1)
+
+            print(
+                f"Getting character info ...({request_count + 1}/{len(character_ids)})",
+                end="\r",
+            )
+            character_info = self.get_character_info(character_id)
+            res_info = {"relation": "", "name": "", "name_chs": "", "gender": ""}
+            if "name" in character_info:
+                res_info["name"] = character_info["name"]
+
+            if "gender" in character_info and character_info["gender"]:
+                res_info["gender"] = character_info["gender"]
+
+            if "infobox" in character_info and isinstance(
+                character_info["infobox"], list
+            ):
+                for info_dict in character_info["infobox"]:
+                    if info_dict["key"] == "简体中文名":
+                        res_info["name_chs"] = info_dict["value"]
+                    elif info_dict["key"] == "性别":
+                        res_info["gender"] = info_dict["value"]
+
+            if character_id in id_to_relation:
+                res_info["relation"] = id_to_relation[character_id]
+
+            result.append(res_info)
+
+            request_count += 1
+
+        print()
+
+        return result
--- konnyaku/utils.py
+++ konnyaku/utils.py
@@ -3,6 +3,7 @@
 """
 
 from konnyaku.errors import RateLimitException, TRLimitException
+from konnyaku.bangumi import BangumiAPI
 from openai import APIError
 
 TPM_RPM_LIMIT_KEYWORDS = ["TPM", "RPM"]
@@ -35,3 +36,35 @@ def limit_exception_raiser(e: Exception | str, is_429=False) -> None:
         raise TRLimitException(e)
 
     raise APIError(e)
+
+
+def extract_bangumi_info(subject_id: str) -> str:
+    """
+    从 Bangumi API 中提取番剧信息，给 LLM 提供背景信息
+
+    :param subject_id: 番剧 ID
+    :return: 番剧信息字串
+    """
+    api = BangumiAPI()
+    subject_info = api.get_subject_info(subject_id)
+    character_infos = api.get_subject_all_character_infos(subject_id)
+    result = ""
+
+    # 基本信息
+    if "name" in subject_info:
+        chs_name = subject_info["name_cn"] if "name_cn" in subject_info else ""
+        result += f"《{subject_info['name']}》({chs_name})\n"
+    if "platform" in subject_info:
+        result += f"<平台>{subject_info['platform']}</平台>\n"
+    if "summary" in subject_info:
+        result += f"<简介>\n{subject_info['summary']}\n</简介>\n"
+
+    result += "\n"
+
+    # 角色信息
+    if len(character_infos) > 0:
+        result += "角色(日文名 / 中文名 / 角色分类 / 性别)\n----------------\n"
+        for character in character_infos:
+            result += f"({character['name']} / {character['name_chs']} / {character['relation']} / {character['gender']})\n"
+
+    return result
